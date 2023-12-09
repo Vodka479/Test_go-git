f195cba7ce0ca258dab8af49cd42af215bd3ad88
VODKA-PC\songw <songwut2015@hotmail.com>
2023-12-09 20:49:12 +0700

    feature :sparkles: add dev

--- main.go
+++ main.go
@@ -6,6 +6,25 @@ import "fmt"
 // 2 commit
 // 3 push
 
+func sub(a, b int) int {
+	return a - b
+}
+
+func sum(a, b int) int {
+	return a + b
+}
+
+func mul(a, b int) int {
+	return a * b
+}
+
+func dev(a, b float64) float64 {
+	return a / b
+}
 func main() {
 	fmt.Println("Hello Github")
+	fmt.Println(sum(1, 2))
+	fmt.Println(sub(1, 2))
+	fmt.Println(mul(1, 2))
+	fmt.Println(dev(1, 2))
 }
